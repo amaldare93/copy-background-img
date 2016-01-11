87d1a8d002e07d5d5f7c2f6c07b8b40aa6766bb7
Anthony Maldarelli <amaldare@iMacarelli.fios-router.home>
2016-01-10 20:24:48 -0500

    add gulp and eslint, event listener

--- background.js
+++ background.js
@@ -1,7 +1,23 @@
+function onClickHandler(info, tab){
+
+  console.log(info);
+  //
+  // var url = $(event.target).css('background-image');
+  // if (url != 'none') {
+  //   console.log(url.slice(5,-2));
+  // }
+}
+
+// attach function to menu button
+chrome.contextMenus.onClicked.addListener(onClickHandler);
+
+// create context menu button
 chrome.runtime.onInstalled.addListener(function(){
 
   chrome.contextMenus.create({
-    "title": "Copy Background Image"
+    'title': 'Copy Background Image',
+    'id': 'contextButton',
+    'contexts': ['all']
   });
-  
+
 });
--- content.js
+++ content.js
@@ -1,7 +1,1 @@
-$("body").on("click", function(event){
-  var url = $(event.target).css("background-image");
-  if (url != "none") {
-    console.log(url.slice(5,-2));
-  }
-});
-console.log('hello');
+// $('body').on('click', );
